19411f9e489416df3afc0df45b101af9c5ed8d4a
Saulftobias <saulftobias@gmail.com>
2021-03-27 20:05:01 +0100

    Alarma, Alarmas y señales

--- practica3/src/main/java/practica3/Alarmas.java
+++ practica3/src/main/java/practica3/Alarmas.java
@@ -3,50 +3,88 @@ package practica3;
 import java.util.*;
 
 public class Alarmas {
-	
+
 	private List<Alarma> alarmasDesactivadas = new LinkedList<Alarma>();
 	private Queue<Alarma> alarmasActivas = new PriorityQueue<Alarma>();
-	
+	private List<Alarma> alarmas = new ArrayList<Alarma>();
+
+	@SuppressWarnings("unused")
 	private AlarmasEstado state;
-	
-	public Alarma alarma(String id) {
+	public Alarmas() { 
+		//TODO Implementar metodo init en la calse AlarmasEstado
+		state = AlarmasEstado.init(this);
+	}
+
+	public Alarma getAlarma(String id) {
+		Alarma buscada;
+		for (int i = 0; i < alarmas.size(); i++) {
+			buscada = alarmasDesactivadas.get(i);
+			if (buscada.getId() == id) return buscada;  
+		}
 		return null;
 	}
-	
+
 	public boolean anhadeAlarma(Alarma a) {
+		alarmas.add(a);
 		return alarmasDesactivadas.add(a);
 	}
-	
+
 	public boolean eliminaAlarma(Alarma a) {
-		return alarmasDesactivadas.remove(a);
+		alarmasActivas.remove(a);
+		alarmasDesactivadas.remove(a);
+		return alarmas.remove(a);
 	}
-	
+
 	public Alarma alarmaMasProxima() {
 		return alarmasActivas.peek();
 	}
-	
+
 	public void desactivaAlarma(Alarma a) {
 		alarmasActivas.remove(a);
 		alarmasDesactivadas.add(a);
 	}
-	
+
 	public void setState(AlarmasEstado state) {
 		this.state = state;
 	}
 
 	public void elimimnaAlarma(String id) {
-
+		Alarma alarma = getAlarma(id);
+		alarmasDesactivadas.remove(alarma);
+		alarmasActivas.remove(alarma);
+		alarmas.remove(alarma);
 	}
 
 	public void activaAlarma(Alarma alarma) {
-		
+		alarmasDesactivadas.remove(alarma);
+		alarmasActivas.add(alarma);
 	}
 
-	public void activaMelodia() {
-		
+	public void activaMelodia(Alarma alarma) {
+		alarma.activa();
 	}
 
-	public void desactivaMelodia() {
-		
+	public void desactivaMelodia(Alarma alarma) {
+		alarma.desactiva();
 	}
-}
+	
+	public void nuevaAlarma(Alarma alarma) {
+		state.nuevaAlarma(this, alarma.getId(), alarma.getHora());
+	}
+	
+	public void borraAlarma(Alarma alarma) {
+		state.borraAlarma(this, alarma.getId());
+	}
+	
+	public void apagar () {
+		state.apagar(this);
+	}
+	
+	public void alarmaOff (Alarma alarma) {
+		state.alarmaOff(this, alarma.getId());
+	}
+	
+	public void alarmaOn (Alarma alarma) {
+		state.alarmaOn(this, alarma.getId());
+	}
+ }
