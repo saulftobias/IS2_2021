19312be1a47b16ac7f71901697f906b2983bc3c3
saulftobias <76536317+saulftobias@users.noreply.github.com>
2021-04-10 10:22:14 +0200

    Delete practica1b directory

--- practica1b/src/main/java/es/unican/is2/practica1b/Main.java
+++
@@ -1,8 +0,0 @@
-package es.unican.is2.practica1b;
-import es.unican.is2.practica1a.Calculadora;
-public class Main {
-	public static void main (String[] args) {
-		int suma = Calculadora.suma(1, 2);
-		System.out.println("El resultado es: " + suma);
-	}
-}
