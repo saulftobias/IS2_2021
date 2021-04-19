013835bddd172ec481c59e1b7be865ebdc57c876
Álvaro López García <alvarolopez769@gmail.com>
2021-04-19 19:24:19 +0200

    Creada clase test JUnit

---
+++ practica4/src/test/java/Modelo/SeguroTest.java
@@ -0,0 +1,14 @@
+package Modelo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SeguroTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
